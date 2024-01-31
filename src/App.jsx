1616d508807b819e5c3d25d4e12af53127e89635
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-31 16:36:56 +0100

    tldraw/assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // this was installed with `npm i @tldraw/tldraw@2.0.0-beta.2`
 
 import { Tldraw } from "@tldraw/tldraw";
+import { getAssetUrls } from "@tldraw/assets/selfHosted";
+
+// based on https://discord.com/channels/859816885297741824/1197142604106698933/1202273417357692950
 import "@tldraw/tldraw/tldraw.css";
 
+const assetUrls = getAssetUrls();
+
 export default function App() {
-  const assetUrls = "/public"
   return (
     <div
       style={{
